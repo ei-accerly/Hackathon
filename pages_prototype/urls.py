23b2fc9eb6997cc129f880081251ab1c22ee361d
ei-accerly <ei.accerly@gmail.com>
2023-07-15 15:59:39 +0800

    landing page set image

--- pages_prototype/urls.py
+++ pages_prototype/urls.py
@@ -10,7 +10,6 @@ from . import views
 
 urlpatterns = [
     path('', views.LandingPage.as_view(), name='landing'),
-    path('1', views.LandingPage1.as_view(), name='landing1'),
     path('destination/<str:country>-<str:place>/', views.DestinationPage.as_view(), name='destination'),
     path('destination/<str:country>-<str:place>', views.DestinationPage.as_view(), name='destination'),
     path('destination/<str:country>-<str:place>/stay', views.HotelPage.as_view(), name='hotel'),
