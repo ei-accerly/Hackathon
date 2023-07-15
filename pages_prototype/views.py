1da191c0d16a37f788d8e0e04386d744e85fb5a1
ei-accerly <ei.accerly@gmail.com>
2023-07-15 11:08:47 +0800

    backend for destinations

--- pages_prototype/views.py
+++ pages_prototype/views.py
@@ -1,7 +1,66 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import requests
 
 # Create your views here.
 
 class LandingPage(TemplateView):
     template_name = 'landingPage.html'
+
+    def post(self, request):
+        def fetch_popular_food(country, city):
+            api_key = "Q2-GLImBqseGs8AIRNI4Vxayyhxul90CWrZgfEyvLa9o-qv59S-ADQMxgAbHXL9P_1PuQBvVbM9SzdfMzH7EQntdh4_K1kD6FotzNm8T29MHkGKUvvO9DPwAswuyZHYx"
+            url = f"https://api.yelp.com/v3/businesses/search"
+
+            headers = {
+                "Authorization": f"Bearer {api_key}"
+            }
+
+            params = {
+                "categories": "restaurants",
+                "location": f"{city}, {country}",
+                "sort_by": "rating",
+                "limit": 5 
+            }
+
+            response = requests.get(url, headers=headers, params=params)
+            data = response.json()
+
+            if "businesses" in data:
+                businesses = data["businesses"]
+                popular_food = [business["name"] for business in businesses]
+                return popular_food
+
+            return []
+
+
+        def fetch_articles(country, query):
+            query = "Tourism in " + query
+            api_key = "a66fd9d712964e8b9e35342520764d7e"
+            url = f"https://newsapi.org/v2/top-headlines?country={country}&q={query}&apiKey={api_key}"
+
+            response = requests.get(url)
+            data = response.json()
+
+            if data['status'] == 'ok':
+                articles = data['articles']
+                return articles
+
+            return []
+
+        # Display the fetched articles
+        if request.POST.get('choose_country'):
+
+            country = request.POST['choose_country']
+            place = request.POST['place']
+
+            articles = fetch_articles(country, place)
+            foods = fetch_popular_food(country, place)
+            for food in foods:
+                print(food)
+            for article in articles:
+                title = article['title']
+                description = article['description']
+                print(f"Title: {title}")
+                print(f"Description: {description}")
+                print("-----")
